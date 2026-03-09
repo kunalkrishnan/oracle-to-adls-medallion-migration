b14002d0f998a6d040baa61bd272e66ea6e47476
kunal krishnan <61145182+kunalkrishnan@users.noreply.github.com>
2026-03-09 16:44:48 +0530

    Update gold_aggregation.sql

--- gold/gold_aggregation.sql
+++ gold/gold_aggregation.sql
@@ -1,10 +1,7 @@
-CREATE TABLE gold_sales
-USING DELTA
-AS
-
 SELECT
 customer_id,
+customer_name,
 SUM(amount) total_sales,
-COUNT(order_id) total_orders
-FROM delta.`/mnt/adls/silver/sales_clean`
-GROUP BY customer_id;
+RANK() OVER (ORDER BY SUM(amount) DESC) AS sales_rank
+FROM sales_enriched
+GROUP BY customer_id, customer_name;
